ea677c2bdd8800580eea951f6320365aff40ba3a
Akebono7 <104069509+Akebono7@users.noreply.github.com>
2022-04-29 22:33:45 +0200

    Update AutputRow.h

--- AutputRow.h
+++ AutputRow.h
@@ -12,7 +12,7 @@ private:
 public:
     AutputRow(uint8_t ROW1, uint8_t ROW2, uint8_t ROW3, uint8_t ROW4, uint8_t ROW5, uint8_t ROW6, uint8_t ROW7, uint8_t ROW8,ShiftRegister* sa); 
     void setPosShiftRegisterRGB(byte p1, byte p2, byte p3, byte p4, byte p5, byte p6, byte p7, byte p8,int f);
-
+    void setAusgenge(byte a, byte b, byte c, byte d, byte e, int pos, int farbe);
 };
 
 
