73eca477439086189d7c74c78067dbcee2b62ca1
Akebono7 <104069509+Akebono7@users.noreply.github.com>
2022-05-03 11:31:14 +0200

    Update Akebono.cpp

--- CPP/Akebono.cpp
+++ CPP/Akebono.cpp
@@ -180,3 +180,78 @@ void Akebono::setZ(int pos, int farbe){
     byte d=0b01111001;
     _autputRow->setAusgenge(a, b, c, d, 0b0, pos, farbe);    
 }
+
+void Akebono::charakter(char a, int farbe, int pos){
+    if(a=='a'){
+        Akebono::setA(pos, farbe);
+    }else if(a=='b'){
+        Akebono::setB(pos, farbe);
+    }else if(a=='c'){
+        Akebono::setC(pos, farbe);
+    }else if(a=='d'){
+        Akebono::setD(pos, farbe);
+    }else if(a=='e'){
+        Akebono::setE(pos, farbe);
+    }else if(a=='f'){
+        Akebono::setF(pos, farbe);
+    }else if(a=='g'){
+        Akebono::setG(pos, farbe);
+    }else if(a=='h'){
+        Akebono::setH(pos, farbe);
+    }else if(a=='i'){
+        Akebono::setI(pos, farbe);
+    }else if(a=='j'){
+        Akebono::setJ(pos, farbe);
+    }else if(a=='k'){
+        Akebono::setK(pos, farbe);
+    }else if(a=='l'){
+        Akebono::setL(pos, farbe);
+    }else if(a=='m'){
+        Akebono::setM(pos, farbe);
+    }else if(a=='n'){
+        Akebono::setN(pos, farbe);
+    }else if(a=='o'){
+        Akebono::setO(pos, farbe);
+    }else if(a=='p'){
+        Akebono::setP(pos, farbe);
+    }else if(a=='q'){
+        Akebono::setQ(pos, farbe);
+    }else if(a=='r'){
+        Akebono::setR(pos, farbe);
+    }else if(a=='s'){
+        Akebono::setS(pos, farbe);
+    }else if(a=='t'){
+        Akebono::setT(pos, farbe);
+    }else if(a=='u'){
+        Akebono::setU(pos, farbe);
+    }else if(a=='v'){
+        Akebono::setV(pos, farbe);
+    }else if(a=='w'){
+        Akebono::setW(pos, farbe);
+    }else if(a=='x'){
+        Akebono::setX(pos, farbe);
+    }else if(a=='y'){
+        Akebono::setY(pos, farbe);
+    }else if(a=='z'){
+        Akebono::setZ(pos, farbe);
+    }
+}
+
+void Akebono::setString(String a, int farbe){
+    int l =a.length();
+    l=l*6;
+    a.toLowerCase();
+    int timer;
+    for(int i=1; i<=l; i++){
+        timer=millis();
+        while (timer+500>millis()){
+          for(int h=0; h <=l; h=h+6){
+              int k=l-h;
+              k=k/6;
+              char c=a.charAt(k);
+              Akebono::charakter(c, farbe, i-h);
+          }
+        }
+        
+    }
+}
