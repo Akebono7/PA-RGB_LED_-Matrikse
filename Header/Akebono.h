ce7e98cf4f77d8d6f616602a5e9c43be45c03ff8
Akebono7 <104069509+Akebono7@users.noreply.github.com>
2022-05-05 11:04:56 +0200

    Update Akebono.h

--- Header/Akebono.h
+++ Header/Akebono.h
@@ -9,6 +9,10 @@ class Akebono{
     void charakter(char a, int farbe, int pos);
     void setSpace(int pos, int farbe);
     public:
+    /*!
+    *   @brief Der construktor der Akebono Klasse bekomt ein objekt id eines 
+    *   Objektes der AutputRow Klasse.(&objekt);
+    */
     Akebono(AutputRow* autputRow);
     /*! @brief setzt A als ausgabe pos gipt die postion des zeichens ann werte von 1 bis 12.
     *   farbe end scheidet welche angesteuert wirt. 0=ROT 1=Grühen 2=Blau.
