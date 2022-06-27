27613d483f258a37b75571f8c8411405a6145a15
Akebono7 <104069509+Akebono7@users.noreply.github.com>
2022-06-27 10:43:16 +0200

    Update ShiftRegister.cpp

--- CPP/ShiftRegister.cpp
+++ CPP/ShiftRegister.cpp
@@ -1,30 +1,32 @@
 #include<Arduino.h>
 #include<ShiftRegister.h>
 
-ShiftRegister::ShiftRegister(uint8_t CLK, uint8_t RCLK, uint8_t OEL,uint8_t SERIN, uint8_t RESTL){
+ShiftRegister::ShiftRegister(uint8_t CLK, uint8_t RCLK, uint8_t OEL,uint8_t SERIN, uint8_t RESTL){      // Konstruktor
     _CLK = CLK;
-    _RCLK = RCLK;
+    _RCLK = RCLK;                       // übergabe der pins an die privaten definds.
     _OEL = OEL;
     _SERIN = SERIN;
     _RESTL = RESTL;
-    pinMode(_CLK,OUTPUT);
+    pinMode(_CLK,OUTPUT);               // vestlegen der pins 
     pinMode(_RCLK,OUTPUT);
     pinMode(_OEL,OUTPUT);
     pinMode(_SERIN,OUTPUT);
     pinMode(_RESTL,OUTPUT);
 
-    digitalWrite(_RESTL, HIGH);
+    digitalWrite(_RESTL, HIGH);         //Start werte festlegen.
     digitalWrite(_OEL, HIGH);
     digitalWrite(_CLK, LOW);
     digitalWrite(_RCLK, LOW);
     digitalWrite(_SERIN, LOW);
 }
-void ShiftRegister::clock(uint8_t a){
+
+
+void ShiftRegister::clock(uint8_t a){       // Erstelen eins clocks signals.
     digitalWrite(a, HIGH);
     digitalWrite(a, LOW);
 }
 
-void ShiftRegister::inputHigh(int a){
+void ShiftRegister::inputHigh(int a){   // HIGH wert einlesen mit 1 als defloltwert.
 
     digitalWrite(_SERIN, HIGH);
     for(int i=0; i < a; i++){
@@ -34,7 +36,7 @@ void ShiftRegister::inputHigh(int a){
 
 }
 
-void ShiftRegister::inputLow(int a){
+void ShiftRegister::inputLow(int a){     // LOW wert einlesen mit 1 als defloltwert.  
 
     digitalWrite(_SERIN, LOW);
     for(int i=0; i < a; i++){
@@ -43,20 +45,20 @@ void ShiftRegister::inputLow(int a){
 }
     
 
-void ShiftRegister::setData(){
+void ShiftRegister::setData(){      //set the valus to the next fliplop.
 
     ShiftRegister::clock(_RCLK);
 
 }
 
-void ShiftRegister::autputRowX(uint8_t a){
+void ShiftRegister::autputRowX(uint8_t a){  // ausgabe reihe über transitor möglich.
     digitalWrite(a, HIGH);
     digitalWrite(_OEL, LOW);
     delayMicroseconds(1);
     digitalWrite(_OEL, HIGH);
     digitalWrite(a, LOW);
 }
-void ShiftRegister::resat(){
+void ShiftRegister::resat(){            // RESAT stetz alle werte zurük.
 
     digitalWrite(_RESTL, LOW);
     digitalWrite(_RESTL, HIGH);
